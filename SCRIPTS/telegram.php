c17179d4743288d6bb3cf86e4d17927396e2d16b
Igor Parfenov <lloopy123123543@gmail.com>
2022-05-25 23:16:04 +0500

    bot

---
+++ SCRIPTS/telegram.php
@@ -0,0 +1,28 @@
+<?php
+
+/* https://api.telegram.org/bot5311375404:AAE_UZ5CvcTZqohhMlvLirT7ASsGNS86BHw/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+$token = "5311375404:AAE_UZ5CvcTZqohhMlvLirT7ASsGNS86BHw";
+$chat_id = "-777341782";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Error";
+}
+?>
